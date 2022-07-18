d033597ca301a8db5790fd17566710bc9cb49679
Dzmitry <maddal.privod@gmail.com>
2022-07-18 11:10:52 +0300

    Add background image and refactoring styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -10,21 +10,25 @@ export const App = () => {
 	const isRegistration = useSelector<AppRootStateType, boolean>(state => state.registration.isRegistration)
 
 	return (
-		<div className={s.app}>
-			<div className={s.container}>
-				<h1 className={s.title}>Sign up</h1>
+		<div className={s.image}>
+			<div className={s.app}>
+				<div className={s.container}>
+					<h1 className={s.title}>Sign up</h1>
+				</div>
+				{!isRegistration &&
+					<div className={s.box_registration}>
+						<RegistrationForm/>
+					</div>
+				}
+				{isRegistration &&
+					<div>
+						<SuccessfulRegistrationPage/>
+					</div>
+				}
 			</div>
-			{!isRegistration &&
-				<div className={s.box_registration}>
-					<RegistrationForm/>
-				</div>
-			}
-			{isRegistration &&
-				<div>
-					<SuccessfulRegistrationPage/>
-				</div>
-			}
 		</div>
+
+
 	)
 }
 
